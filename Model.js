1e0d629c088cc998403eaf808a0a1e4b21171b20
Dmitry Chuiko <dmitrychuiko03@gmail.com>
2025-06-03 23:38:15 +0300

    Complete control task

--- Model.js
+++ Model.js
@@ -1,120 +1,61 @@
+export class Model {
 
-
-function deg2rad(angle) {
-    return angle * Math.PI / 180;
-}
-
-
-function Vertex(p)
-{
-    this.p = p;
-    this.normal = [];
-    this.triangles = [];
-}
-
-function Triangle(v0, v1, v2)
-{
-    this.v0 = v0;
-    this.v1 = v1;
-    this.v2 = v2;
-    this.normal = [];
-    this.tangent = [];
-}
-
-// Constructor
-function Model(name) {
-    this.name = name;
-    this.iVertexBuffer = gl.createBuffer();
-    this.iIndexBuffer = gl.createBuffer();
-    this.count = 0;
-
-    this.BufferData = function(vertices, indices) {
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STREAM_DRAW);
-        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shProgram.iAttribVertex);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STREAM_DRAW);
-
-        this.count = indices.length;
+    constructor() {
+        this.positions = [];
+        this.uvs = [];
+        this.indices = [];
+        this.stepsU = 50;
+        this.stepsV = 40;
     }
 
-    this.Draw = function() {
+    CalculateCoordinateVertex = function(u, v) {
+        let x = (-3 * u - u ** 5 + 2 * u ** 3 * v ** 2 + 3 * u * v ** 4) / (6 * (u ** 2 + v ** 2));
+        let y = (-3 * v - 3 * u ** 4 * v - 2 * u ** 2 * v ** 3 + v ** 5) / (6 * (u ** 2 + v ** 2));
+        let z = u;
 
-        //gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-        gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
+        return [x, y, z]
     }
 
-    this.DrawWireframe = function() {
+    CreateSurface = function () {
+        const uMin = -1, uMax = 1;
+        const vMin = 0.2, vMax = 1;
+        const stepU = (uMax - uMin) / this.stepsU;
+        const stepV = (vMax - vMin) / this.stepsV;
 
-        for (let p=0; p<this.count; p+=3)                    // offset in bytes (UNSIGNED_SHORT is two bytes)
-            gl.drawElements(gl.LINE_LOOP, 3, gl.UNSIGNED_SHORT, p*2);
-    }
-}
+        const uCount = Math.floor((uMax - uMin) / stepU) + 1;
+        const vCount = Math.floor((vMax - vMin) / stepV) + 1;
 
+        for (let i = 0; i < uCount; i++) {
+            const u = uMin + i * stepU;
+            for (let j = 0; j < vCount; j++) {
+                const v = vMin + j * stepV;
 
-function CreateSurfaceData(data)
-{
-    let vertices = [];
-    let triangles = [];
+                const [x, y, z] = this.CalculateCoordinateVertex(u, v);
 
-    for (let i=0, ang = 0; i<72; i++, ang+=5) {
-        vertices.push( new Vertex( [Math.sin(deg2rad(ang)), 0, Math.cos(deg2rad(ang))] ));
-    }
+                this.positions.push(x, y, z);
 
-    for (let i=0, ang = 0; i<72; i++, ang+=5) {
-
-        let v0ind = vertices.length;
-        vertices.push( new Vertex( [Math.sin(deg2rad(ang)), 1, Math.cos(deg2rad(ang))] ));
-
-        // v0    v2 
-        //   o - o
-        //   | \ |
-        //   o - o
-        // v3     v1
-
-        if (i > 0)
-        {
-            let v1ind = v0ind - 72 -1;
-            let v2ind = v0ind - 1;
-            let v3ind = v0ind - 72
-
-            let trian = new Triangle(v0ind, v1ind, v2ind);
-            let trianInd = triangles.length;
-
-            triangles.push( trian );
-            vertices[v0ind].triangles.push(trianInd);
-            vertices[v1ind].triangles.push(trianInd);
-            vertices[v2ind].triangles.push(trianInd);
-
-            let trian2 = new Triangle(v0ind, v3ind, v1ind);
-            let trianInd2 = triangles.length;
-
-            triangles.push( trian2 );
-            vertices[v0ind].triangles.push(trianInd2);
-            vertices[v3ind].triangles.push(trianInd2);
-            vertices[v1ind].triangles.push(trianInd2);
-
+                const uTex = (u - uMin) / (uMax - uMin);
+                const vTex = (v - vMin) / (vMax - vMin);
+                this.uvs.push(uTex, vTex);
+            }
         }
 
-    }
+        for (let i = 0; i < uCount - 1; i++) {
+            for (let j = 0; j < vCount - 1; j++) {
+                const idx = i * vCount + j;
+                const idxNextU = (i + 1) * vCount + j;
 
-    data.verticesF32 = new Float32Array(vertices.length*3);
-    for (let i=0, len=vertices.length; i<len; i++)
-    {
-        data.verticesF32[i*3 + 0] = vertices[i].p[0];
-        data.verticesF32[i*3 + 1] = vertices[i].p[1];
-        data.verticesF32[i*3 + 2] = vertices[i].p[2];
-    }
+                this.indices.push(idx, idxNextU, idx + 1);
+                this.indices.push(idxNextU, idxNextU + 1, idx + 1);
+            }
+        }
 
-    data.indicesU16 = new Uint16Array(triangles.length*3);
-    for (let i=0, len=triangles.length; i<len; i++)
-    {
-        data.indicesU16[i*3 + 0] = triangles[i].v0;
-        data.indicesU16[i*3 + 1] = triangles[i].v1;
-        data.indicesU16[i*3 + 2] = triangles[i].v2;
+        const geo = new THREE.BufferGeometry();
+        geo.setAttribute("position", new THREE.Float32BufferAttribute(this.positions, 3));
+        geo.setAttribute("uv", new THREE.Float32BufferAttribute(this.uvs, 2));
+        geo.setIndex(this.indices);
+        geo.computeVertexNormals();
+        return geo;
     }
 
 }
